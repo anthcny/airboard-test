23dc92390a4ade151f79ad153d3f6e4a55273a42
Anthony <anthony@Anthonys-MacBook-Air.local>
2018-11-16 13:34:09 +0300

    styles correct

--- src/airboard/app.js
+++ src/airboard/app.js
@@ -6,6 +6,8 @@ import {createTableHTML, saveTableData, tableData, searchInTable} from './tableW
 
 /**
  * Поскольку API не предоставляет данных о задержанных рейсах, сделал только вывод информации по отправляющимся и прибывающим рейсам.
+ * Ссылка на документацию к API https://tech.yandex.ru/rasp/doc/reference/schedule-point-point-docpage/
+ * Также не увидел возможности отправлять запрос по номеру рейса, поэтому поиск ведется среди полученных рейсов.
  */
 
 export function app(){
